96cd275edef14c0cb4451cb40b2d08b3b547cbf5
Karan <karan.antham@gmail.com>
2014-09-10 00:57:35 +0200

    added shelf, basic look

--- js/app.js
+++ js/app.js
@@ -3,18 +3,19 @@
 'use strict';
 
 // angular.js main app initialization
-var app = angular.module('layout', []).
-        config(['$routeProvider', function($routeProvider) {
+var app = angular.module('layout', [])
+        .config(['$routeProvider', function($routeProvider) {
                 $routeProvider.
                         when('/', {templateUrl: 'pages/library.html', controller: LibCtrl, activetab: 'library'}).
                         when('/mypages', {templateUrl: 'pages/mypages.html', controller: MypageCtrl, activetab: 'mypages'}).
                         when('/logout', {templateUrl: 'pages/logout.html', controller: LogoutCtrl, activetab: 'logout'}).
                         otherwise({redirectTo: '/'});
-            }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function($scope, $http, $browser, $timeout, $route) {
+            }])
+        .run(['$rootScope', '$http', '$browser', '$timeout', "$route", function($scope, $http, $browser, $timeout, $route) {
 
-        $scope.$on("$routeChangeSuccess", function(scope, next, current) {
-            $scope.part = $route.current.activetab;
-        });
+                $scope.$on("$routeChangeSuccess", function(scope, next, current) {
+                    $scope.part = $route.current.activetab;
+                });
 
 //        // onclick event handlers
 //        $scope.showForm = function () {
@@ -24,7 +25,7 @@ var app = angular.module('layout', []).
 //          $('.contactRow').slideUp();
 //        };
 
-        // save the 'Contact Us' form
+                // save the 'Contact Us' form
 //        $scope.save = function () {
 //          $scope.loaded = true;
 //          $scope.process = true;
@@ -33,7 +34,7 @@ var app = angular.module('layout', []).
 //              $scope.process = false;
 //          });
 //        };
-    }]);
+            }]);
 
 app.config(['$locationProvider', function($location) {
         $location.hashPrefix('!');
